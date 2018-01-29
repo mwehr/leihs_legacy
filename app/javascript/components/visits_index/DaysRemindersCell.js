536a4dff7c87a2c4421b07b7f196adbd96dda2bf
Matúš Kmiť <matus.kmit@zhdk.ch>
2018-01-29 13:51:37 +0100

    reminders popup content

--- app/javascript/components/visits_index/DaysRemindersCell.js
+++ app/javascript/components/visits_index/DaysRemindersCell.js
@@ -8,7 +8,8 @@ class DaysRemindersCell extends React.Component {
     super()
     this.state = {
       show: false,
-      reminderSent: false }
+      reminderSent: false
+    }
   }
 
   showTooltip() {
@@ -27,10 +28,14 @@ class DaysRemindersCell extends React.Component {
       max_ranges.push(
         moment(reservation.end_date)
           .endOf('day')
-          .diff(moment(reservation.start_date).startOf('day'), 'days'),
+          .diff(moment(reservation.start_date).startOf('day'), 'days')
       )
     }
     return 1 + max_ranges.reduce((a, b) => Math.max(a, b))
+  }
+
+  renderPopup() {
+    return this.props.v.type == 'take_back' && (this.props.reminderSent || this.props.isOverdue)
   }
 
   renderDaysCell() {
@@ -38,46 +43,53 @@ class DaysRemindersCell extends React.Component {
 
     let content
     if (this.props.v.type == 'take_back' && this.props.reminderSent) {
-      content =
+      content = (
         <div className="latest-reminder-cell text-align-center tooltipstered">
           <strong>{`${_jed('Reminder sent')} `}</strong>
           <i className="fa fa-envelope" />
         </div>
+      )
     } else if (this.props.v.type == 'take_back' && this.props.isOverdue) {
-      content =
+      content = (
         <div className="latest-reminder-cell text-align-center">
-          {_jed("Latest reminder")}
-          <i className='fa fa-envelope-alt'></i>
+          {_jed('Latest reminder')}
+          <i className="fa fa-envelope-alt" />
         </div>
+      )
     } else {
-      content =
+      content = (
         <div className="text-align-center">
           {maxDays} {_jed(maxDays, 'day', 'days')}
         </div>
+      )
     }
 
     return content
   }
 
   diffToday(date) {
-    if (moment().startOf("day").diff(moment(date).startOf("day"), "days") == 0) {
-      return _jed("Today")
+    if (
+      moment()
+        .startOf('day')
+        .diff(moment(date).startOf('day'), 'days') == 0
+    ) {
+      return _jed('Today')
     } else {
-      return moment(date).startOf("day").from(moment().startOf("day"))
+      return moment(date)
+        .startOf('day')
+        .from(moment().startOf('day'))
     }
   }
 
   renderNotifications() {
-    const dateAndTime = (date) => {
-      return (
-        `${this.diffToday(date)} ${moment(date).format("LT")}`
-      )
+    const dateAndTime = date => {
+      return `${this.diffToday(date)} ${moment(date).format('LT')}`
     }
 
-    return f.map(this.props.v.notifications, (n) => {
+    return f.map(this.props.v.notifications, n => {
       return (
-        <div key={n.id} className='row width-l'>
-          <div className='paragraph-s'>
+        <div key={n.id} className="row width-l">
+          <div className="paragraph-s">
             <strong>{dateAndTime(this.props.v.date)}</strong>
             {` ${n.title}`}
           </div>
@@ -87,34 +99,40 @@ class DaysRemindersCell extends React.Component {
   }
 
   render() {
-    return [
+    return (
       <div
-        className='col1of5 line-col'
+        className="col1of5 line-col"
         key={`reminder-${this.props.v.id}`}
         ref={`reminder-${this.props.v.id}`}
         onMouseEnter={this.showTooltip.bind(this)}
-        onMouseLeave={this.hideTooltip.bind(this)}
-      >
-        {this.renderDaysCell()}
-      </div>,
-      <Overlay
-        show={this.state.show}
-        animation={false}
-        id={`reminder-overlay-${this.props.v.id}`}
-        key={`reminder-overlay-${this.props.v.id}`}
-        placement="top"
-        target={() =>
-          ReactDOM.findDOMNode(this.refs[`reminder-${this.props.v.id}`])
-        }
-      >
-        <Popover
-          id={`reminder-popover-${this.props.v.id}`}
-          key={`reminder-popover-${this.props.v.id}`}
-        >
-          {this.renderNotifications()}
-        </Popover>
-      </Overlay>,
-    ]
+        onMouseLeave={this.hideTooltip.bind(this)}>
+        {this.renderPopup() && (
+          <Popup popupRef={this.popup}>
+            <div
+              style={{ opacity: '1' }}
+              className="tooltipster-sidetip tooltipster-default tooltipster-top tooltipster-initial"
+              id="tooltipster-480797"
+              style={{
+                pointerEvents: 'auto',
+                zIndex: '9999999',
+                left: '376px',
+                top: '142px',
+                height: '108px',
+                width: '322px',
+                animationDuration: '350ms',
+                transitionDuration: '350ms'
+              }}>
+              <div className="tooltipster-box">
+                <div className="tooltipster-content">
+                  <div className="min-width-l">{this.renderNotifications()}</div>
+                </div>
+              </div>
+            </div>
+          </Popup>
+        )}
+        <div ref={ref => (this.popup = ref)}>{this.renderDaysCell()}</div>
+      </div>
+    )
   }
 }
 
