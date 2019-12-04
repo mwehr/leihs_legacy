4b905dceb7d57a86f4ddc3cd6681262ccdf38011
Matus Kmit <matus.kmit@zhdk.ch>
2019-12-04 13:18:55 +0100

    factories

--- factories/order_factory.rb
+++ factories/order_factory.rb
@@ -1,9 +1,20 @@
+class CustomerOrder < ApplicationRecord
+  belongs_to :user
+end
+
 FactoryGirl.define do
 
   factory :order do
     inventory_pool
     user { FactoryGirl.create(:customer, inventory_pool: inventory_pool) }
     purpose { Faker::Lorem.sentence }
+
+    after(:build) do |order|
+      co = create(:customer_order,
+                  user: order.user,
+                  purpose: order.purpose)
+      order.customer_order_id = co.id
+    end
   end
 
 end
